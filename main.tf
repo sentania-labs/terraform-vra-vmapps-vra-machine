571c62e72ea21c51bcaa82126407b0ab337f1a45
Scott R Bowe <scottb@sentania.net>
2025-12-08 18:57:28 -0600

    .

--- main.tf
+++ main.tf
@@ -3,8 +3,8 @@ data "vra_project" "this" {
 }
 
 resource "vra_machine" "this" {
-  name        = var.virtualmachine_name
-  description = var.virtualmachine_description
+  name        = var.virtual_machine_name
+  description = var.virtual_machine_description
   project_id  = data.vra_project.this.id
   image       = var.image
   flavor      = var.flavor
